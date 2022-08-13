0858b6d8bbac722ebef08281e068d7181a2c9704
NALLAPARAJU KRISHNA RAVITEJA VARMA <35894516+DevopsStudio2022@users.noreply.github.com>
2022-08-13 09:00:43 +0530

    Update airflow_installation.sh

--- airflow_installation.sh
+++ airflow_installation.sh
@@ -9,7 +9,7 @@ PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
 # For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.3.3/constraints-3.7.txt
-pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+sudo pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
 # The Standalone command will initialise the database, make a user and starts all components for you
 airflow standalone
